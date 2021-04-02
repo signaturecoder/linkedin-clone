184c425c23e8ac194cd4233bd62c3f107d12537a
signaturecoder <snkmr13@gmail.com>
2021-04-02 18:23:09 +0530

    first commit folder structure

---
+++ src/app/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Header.css'
+import HeaderOption from './HeaderOption';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="header__left">
+                <img src="https://www.flaticon.com/svg/vstatic/svg/174/174857.svg?token=exp=1617362851~hmac=3b5c06e519f4878d0ac19dd7f6983cd2" alt="" />
+                <div className="header__search">
+                    <SearchIcon />
+                    <input type="text"/>
+                </div>
+            </div>
+            <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title="Home"/>
+                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
+                <HeaderOption avatar="https://media-exp1.licdn.com/dms/image/C5103AQHZzVgL2-g24g/profile-displayphoto-shrink_800_800/0/1571117334679?e=1622678400&v=beta&t=Re25bLczrga6FSIruKn5d-0h2T2vWf20bWlRUxv7sIQ" title="Me"/>
+            </div>
+    
+        </div>
+    )
+}
+
+export default Header
